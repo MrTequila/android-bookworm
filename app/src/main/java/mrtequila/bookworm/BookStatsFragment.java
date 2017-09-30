ba140a66843621b974669f5bd3471de0893032d8
MrTequila <igi444@gmail.com>
2017-09-30 22:28:29 +0200

    Added bar chart to show how many pages you read each month.

--- app/src/main/java/mrtequila/bookworm/BookStatsFragment.java
+++ app/src/main/java/mrtequila/bookworm/BookStatsFragment.java
@@ -1,6 +1,5 @@
 package mrtequila.bookworm;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,9 +7,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Michal on 2017-09-28.
@@ -47,15 +57,64 @@ public class BookStatsFragment extends android.support.v4.app.Fragment {
 
         final ArrayList<Book> booksList = dataSource.getAllBooksArray();
 
-        Book testOne = booksList.get(0);
+        final Map<Integer, Integer> chartData = new TreeMap<Integer, Integer>();
+        List<BarEntry> chartEntry = new ArrayList<>();
 
-        String name = testOne.getAuthor();
+        for (Book book:booksList){
+            String finish = book.getFinishDate();
 
-        TextView nameText = (TextView) getView().findViewById(R.id.stat_info_1);
-        nameText.setText(name);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            Date dateFinishDate = null;
+
+            Calendar finishDateCalendar = Calendar.getInstance();
+
+            // try to parse start and finish date from fields - if not empty
+            // to check if start date is before finish date
+            try {
+                dateFinishDate = simpleDateFormat.parse(finish);
+
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            finishDateCalendar.setTime(dateFinishDate);
+
+            int pagesRead = book.getPageNumber();
+
+            String monthFinished = new SimpleDateFormat("MM").format(dateFinishDate);
+            int monthFinishedInt = Integer.parseInt(monthFinished);
+
+            if (chartData.containsKey(monthFinishedInt)){
+                pagesRead += chartData.get(monthFinishedInt);
+                chartData.put(monthFinishedInt, pagesRead);
+            } else {
+                chartData.put(monthFinishedInt, pagesRead);
+            }
+
+        }
+
+        for (Map.Entry<Integer, Integer> entry : chartData.entrySet() ){
+            chartEntry.add(new BarEntry(entry.getKey(), entry.getValue()));
+            System.out.println("Key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+
+
+        BarDataSet dataSet = new BarDataSet(chartEntry, "pages read");
+
+        BarData barData = new BarData(dataSet);
+        barData.setBarWidth(0.9f);
+
 
         FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
         fab.hide();
+
+        BarChart barChart = (BarChart) getView().findViewById(R.id.pages_chart);
+
+        barChart.setData(barData);
+        barChart.setFitBars(true);
+        barChart.invalidate();
 
     }
 }
